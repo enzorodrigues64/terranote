704a7a3d0adbc3e38da1372adbbf3963ed6430c7
enzorodrigues64 <enzorodrigues2799@gmail.com>
2026-09-18 10:28:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C="terranote-cache-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","style.css","app.js","manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE="terranote-v3";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","style.css","app.js","manifest.json","icon-192.png","icon-512.png"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
